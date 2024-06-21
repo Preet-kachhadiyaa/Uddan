ead2f050fe5b6f34a043aa39e532f0c7ad3e3f39
preet <preet.k02700270@gmail.com>
2024-06-22 01:00:00 +0530

    try forth

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import App from './App.jsx'
 import './index.css'
+import '../src/App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
